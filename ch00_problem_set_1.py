8af17b9d9cfbc9620e7a0a776d486b7a5e19691f
marctaylor <mtaylor@fwparker.org>
2017-01-24 22:02:20 -0600

    Problem set

--- ch00_problem_set_1.py
+++ ch00_problem_set_1.py
@@ -6,8 +6,9 @@
 # 3(60x^2 + 3x/9) + 2x - 4/3(x) - sqrt(x)
 # where x = 12.83
 x = 12.83
-your_answer = "Enter the equation"
+your_answer = float(3 * (60( (x**2) + 3*x/9) + 2*x - 4/3*x - math.sqrt(x)))
 print(your_answer)
+
 
 #PROBLEM 2 (Set your alarm - 3pts)
 
@@ -15,13 +16,25 @@ print(your_answer)
 # You set an alarm to go off 728 hours later.
 # At what time will the alarm go off? Write a program that prints the answer.
 # Hint: for the best solution, you will need the modulo operator.
+time = 1
+next_alarm = 728
 
+alarm_alert= time + next_alarm/24
+print(alarm_alert)
 
 
 #PROBLEM 3 (Wholesale Books - 3pts)
 #The cover price of a book is $27.95, but bookstores get a 50 percent discount.
 #Shipping costs $4 for the first copy and 75 cents for each additional copy.
 # Calculate the total wholesale costs for 68 copies to the nearest penny.
+
+cover_price = 27.95/2
+shipping_price = .75
+first_shipping_price = 4
+
+final_cost =cover_price + (67*shipping_price) + first_shipping_price
+print(final_cost)
+
 
 
 #PROBLEM 4 (Dining Room Chairs - 3pts)
@@ -33,12 +46,19 @@ chair_price = 189.99
 tax_percent = 0.095
 units = 8
 
+chair_total = (chair_price + (chair_price * tax_percent)) * 8
+print(chair_total)
+
 #PROBLEM 5 (Area of Circle - 3pts)
 # Write code that can compute the area of circle.
 # Create variables for radius and pi (3.14159)
 # The formula, in case you do not know, is radius times radius times pi.
 # Print the outcome of your program as follows:
 # “The surface area of a circle with radius ... is ...”
+
+r = 5
+area_equation = math.pi *(r**2)
+print("The surface area of a circle with a radius", r, "is", area_equation)
 
 #PROBLEM 6 (Coin counter - 4pts)
 # Write code that classifies a given amount of money (which you store in a variable named count),
@@ -62,4 +82,7 @@ b = 23
 print( "a =", a, "and b =", b)
 a += b # this is the first line to help you out
 # add two more lines of code here to cause swapping of a and b
+
+b = a + b
+a += b
 print( "a =", a, "and b =", b)
